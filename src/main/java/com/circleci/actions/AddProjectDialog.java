4c372db9ec3e7e7ca8e0d3e1668cb89c93e91dd5
Chris Kowalski <chris.kowalski@circleci.com>
2020-12-28 21:57:15 +0100

    Adding message bundle and action titles

--- src/main/java/com/circleci/actions/AddProjectDialog.java
+++ src/main/java/com/circleci/actions/AddProjectDialog.java
@@ -1,6 +1,8 @@
 package com.circleci.actions;
 
+import com.circleci.i18n.CircleCIBundle;
 import com.circleci.ui.CircleCIDialog;
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
@@ -9,6 +11,12 @@ import org.jetbrains.annotations.NotNull;
  * @author Chris Kowalski
  */
 public class AddProjectDialog extends AnAction {
+
+    public AddProjectDialog() {
+        super(CircleCIBundle.messagePointer("add.project.action"),
+                AllIcons.General.Add);
+    }
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         final CircleCIDialog dialog = new CircleCIDialog(e.getProject());
--- src/main/java/com/circleci/actions/OpenSettingsDialog.java
+++ src/main/java/com/circleci/actions/OpenSettingsDialog.java
@@ -1,5 +1,7 @@
 package com.circleci.actions;
 
+import com.circleci.i18n.CircleCIBundle;
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.options.ShowSettingsUtil;
@@ -9,6 +11,12 @@ import org.jetbrains.annotations.NotNull;
  * @author Chris Kowalski
  */
 public class OpenSettingsDialog extends AnAction {
+
+    public OpenSettingsDialog() {
+        super(CircleCIBundle.messagePointer("open.settings.action"),
+                AllIcons.General.Settings);
+    }
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         ShowSettingsUtil.getInstance().showSettingsDialog(e.getProject(),"CircleCI");
