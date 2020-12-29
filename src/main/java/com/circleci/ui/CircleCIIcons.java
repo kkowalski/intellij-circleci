4f80a4caa867f04e5447f5b51a501a0de346e0a3
Chris Kowalski <chris.kowalski@circleci.com>
2020-12-29 18:29:59 +0100

    Using non-deprecated getIcon method

--- src/main/java/com/circleci/ui/CircleCIIcons.java
+++ src/main/java/com/circleci/ui/CircleCIIcons.java
@@ -8,10 +8,10 @@ import javax.swing.*;
  * @author Chris Kowalski
  */
 public class CircleCIIcons {
-    public static Icon SUCCESS_BUILD = IconLoader.getIcon("/icons/build-status/status-success.svg");
-    public static Icon FAILED_BUILD = IconLoader.getIcon("/icons/build-status/status-failure.svg");
-    public static Icon RUNNING_BUILD = IconLoader.getIcon("/icons/build-status/status-running.svg");
-    public static Icon ON_HOLD = IconLoader.getIcon("/icons/build-status/status-on-hold.svg");
-    public static Icon CANCELED_BUILD = IconLoader.getIcon("/icons/build-status/status-canceled.svg");
-    public static Icon SETUP_NEEDED_BUILD = IconLoader.getIcon("/icons/build-status/status-setup-needed.svg");
+    public static Icon SUCCESS_BUILD = IconLoader.getIcon("/icons/build-status/status-success.svg", CircleCIIcons.class);
+    public static Icon FAILED_BUILD = IconLoader.getIcon("/icons/build-status/status-failure.svg", CircleCIIcons.class);
+    public static Icon RUNNING_BUILD = IconLoader.getIcon("/icons/build-status/status-running.svg", CircleCIIcons.class);
+    public static Icon ON_HOLD = IconLoader.getIcon("/icons/build-status/status-on-hold.svg", CircleCIIcons.class);
+    public static Icon CANCELED_BUILD = IconLoader.getIcon("/icons/build-status/status-canceled.svg", CircleCIIcons.class);
+    public static Icon SETUP_NEEDED_BUILD = IconLoader.getIcon("/icons/build-status/status-setup-needed.svg", CircleCIIcons.class );
 }
